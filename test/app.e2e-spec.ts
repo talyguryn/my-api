a63f0279a8a41d031f0091df7841317bd59538ff
Taly <vitalyguryn@gmail.com>
2026-06-01 02:55:40 +0300

    up

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import * as request from 'supertest';
+import request from 'supertest';
 import configuration from '@/config/configuration';
 import { AppModule } from '@/app.module';
 
--- test/jest-e2e.js
+++ test/jest-e2e.js
@@ -7,6 +7,6 @@ module.exports = {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/../src/$1',
   },
 };
